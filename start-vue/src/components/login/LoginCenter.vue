a3493e89a838ffd91d783568b070ccccb34722ce
PreshineZhou <peixia_6766@163.com>
2017-09-05 00:46:36 +0800

    npm install

--- start-vue/src/components/login/LoginCenter.vue
+++ start-vue/src/components/login/LoginCenter.vue
@@ -3,7 +3,7 @@
   <div class="login-form-wrapper">
       <form class="main-form login-form" method="post" >
           <div class="login-logo-warpper" >
-              <img src="" class="login-logo" >
+              <img src="login-logo.png" class="login-logo" >
           </div>
           <div class="login-form-item" >
               <img src="Email.png" class="login-email-icon" >
@@ -32,6 +32,79 @@ export default {
 </script>
 
 <<style scoped>
-
+.login-background {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    z-index: -100;
+}
+.login-form-wrapper {
+    padding-top: 150px;
+}
+.main-form, .main-form * {
+    box-sizing: border-box;
+}
+.main-form {
+    box-sizing: border-box;
+    width: 360px;
+    max-width: 95%;
+    margin: 0 auto;
+}
+.login-form {
+    background-color: #fff;
+    padding: 40px;
+}
+.login-logo-wrapper {
+    text-align: center;
+}
+.main-form * {
+    display: inline-block;
+}
+.login-form .login-form-form-item {
+    display: block;
+    border: none;
+    border-radius: 3px;
+    background-color: #f5f4f3;
+    padding: 10px 14px;
+    margin: 0 auto 15px;
+}
+.login-form .login-form-item .login-email-icon {
+    margin-bottom: 3px;
+}
+img {
+    vertical-align: middle;
+}
+img {
+    border: 0;
+}
+.login-form .login-form-item .login-form-input {
+    border: none;
+    background-color: #f5f4f3;
+    width: 80%;
+    margin-left: 12px;
+}
+.login-form .login-form-item .login-password-icon {
+    margin: 0 3px 3px;
+}
+.login-form .login-form-item.captcha-item {
+    width: 60%;
+    float: left;
+}
+.login-form .login-form-item.captcha-item .captcha-input {
+    margin: 0;
+}
+.login-form .login-button {
+    display: block;
+    width: 100%;
+    font-size: 18px;
+    background: #e4e40c;
+    border: none;
+    border-radius: 3px;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+.forget-password-link, .register-link {
+    display: block;
+}
 </style>
 
